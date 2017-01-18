0abd2c445ff35333192ff05b9a69051233994cb5
Cameron Egger <trumpetce76@yahoo.com>
2017-01-17 20:04:34 -0600

    Allowed to Open and edit files. Made file view scrollable
    ToDo: Actually save the files. Allow to save custom buttons. make the
    commands work for all commands. make it look better. allow for opening
    multiple files.

--- Window.java
+++ Window.java
@@ -2,14 +2,19 @@ import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
 import java.util.*;
+import java.io.*;
 
 public class Window extends JFrame
 {
 	public static ArrayList<JButton> personalized;
+	private JTextArea edit;
 	private JButton buttonCreator;
+	private JButton open;
+	private JTextField toOpen;
 	private JMenuBar menu;
 	public Window()
 	{
+		
 		personalized = new ArrayList<JButton>();
 		menu = new JMenuBar();
 		add(menu);
@@ -19,6 +24,19 @@ public class Window extends JFrame
 		
 		creationEvent create = new creationEvent();
 		buttonCreator.addActionListener(create);
+		
+		toOpen = new JTextField(25);
+		add(toOpen);
+		
+		open = new JButton("Open File");
+		add(open);
+		
+		openingEvent opener = new openingEvent();
+		open.addActionListener(opener);
+		
+		edit = new JTextArea(50,80);
+		JScrollPane scroller = new JScrollPane(edit);
+		add(scroller);
 		
 		setLayout(new FlowLayout());
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -44,6 +62,28 @@ public class Window extends JFrame
 		public void actionPerformed(ActionEvent e)
 		{
 			ButtonCreator create = new ButtonCreator();
+		}
+	}
+	
+	public class openingEvent implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e)
+		{
+			try
+			{
+				String read = "";
+				Scanner file = new Scanner(new File(toOpen.getText()));
+				while(file.hasNextLine())
+				{
+					read += file.nextLine() + "\n";
+				}
+				edit.setText(read);
+				Execute.update();
+			}
+			catch(Exception E)
+			{
+				System.out.println("File not found");
+			}
 		}
 	}
 }
